5abfc34ee42677326728fafc88e3031d7129fc1c
sattu lahari <sattulahari@gmail.com>
2021-11-09 16:57:07 +0530

    comments

--- Dxc_react/my-app/src/App.js
+++ Dxc_react/my-app/src/App.js
@@ -7,9 +7,15 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 function App() {
   return (
     <div className="App">
+    <NameList/>  
+    <UserGreeting/>  
+    <ParentComponent/> 
     <EventBind/> 
     <ClassClick/>
     <FunctionClick/>
