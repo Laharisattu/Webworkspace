ca02c81590457c80c435a6b37e5c9f5e1add0f1d
sattu lahari <sattulahari@gmail.com>
2021-11-10 17:16:11 +0530

    comment

--- Dxc_react/my-app/src/App.js
+++ Dxc_react/my-app/src/App.js
@@ -10,27 +10,25 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+
 function App() {
   return (
     <div className="App">
+     <h1 className = {styles.success}>success</h1>
+    <h1 className = 'error'>error</h1>
+
+    <Inline/>
+    <Stylesheet  enabled = {true}/>
+    <Table/>  
     <NameList/>  
     <UserGreeting/>  
     <ParentComponent/> 
     <EventBind/> 
-    <ClassClick/>
-    <FunctionClick/>
-    <Counter/>
-    <Message/>
-      <Greet name = "Lahari" college = "AITS">
-        <p> this is children property</p> 
-      </Greet>   
-
-      <Greet name = "Lalli" college = "SVCE">
-        <button> click me</button>
-      </Greet>
-
-      <Greet/>
-      <Welcome name ="Lahari" alias = "Lalli"/>
      
     </div>
   );
